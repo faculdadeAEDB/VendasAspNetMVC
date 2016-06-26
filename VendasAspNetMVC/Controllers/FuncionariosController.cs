616475fe3f6efddb2fce8d9a3dcace9ebe8bc55b
Roberto <robertooliveiraestudo@gmail.com>
2016-06-26 16:43:35 -0300

    First

---
+++ VendasAspNetMVC/Controllers/FuncionariosController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VendasAspNetMVC.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Funcionarios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Funcionarios/Novo
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Novo
+        [HttpPost]
+        public ActionResult Novo(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Funcionarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Funcionarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ VendasAspNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VendasAspNetMVC.MvcApplication" Language="C#" %>
