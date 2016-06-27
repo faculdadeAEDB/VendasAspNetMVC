0aaac6b956bebe4a98867da739b3b1368eaf8cf8
Roberto <robertooliveiraestudo@gmail.com>
2016-06-26 23:22:45 -0300

    Modelos

---
+++ VendasAspNetMVC/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendasAspNetMVC.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string ProNome { get; set; }
+        public int ProQtdEstoque { get; set; }
+        public decimal ProPercLucro{ get; set; }
+    }
+}
