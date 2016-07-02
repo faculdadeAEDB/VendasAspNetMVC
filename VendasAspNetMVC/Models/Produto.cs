e4d5574eaa38779b8053b900d0d316af7d93f8e8
Gustavo <gustavosabonare@hotmail.com>
2016-07-02 16:07:17 -0300

    DataModel added e ProDescricao

--- VendasAspNetMVC/Models/Produto.cs
+++ VendasAspNetMVC/Models/Produto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace VendasAspNetMVC.Models
 {
@@ -10,6 +11,7 @@ namespace VendasAspNetMVC.Models
         public int ID { get; set; }
         public string ProNome { get; set; }
         public int ProQtdEstoque { get; set; }
-        public decimal ProPercLucro{ get; set; }
+        public decimal ProPercLucro { get; set; }
+        public string ProDescricao { get; set; }
     }
 }
